3f93f3d6f671a3f2bd1f88cc1ed9dd2e676aae11
Vinodkumar PANDIYAN <vinodkumarpandiyan@outlook.com>
2021-08-27 23:59:46 +0530

    First commit

---
+++ sts-github-project/src/main/java/com/vp/demo/sts_github_project/App.java
@@ -0,0 +1,7 @@
+package com.vp.demo.sts_github_project;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ sts-github-project/src/test/java/com/vp/demo/sts_github_project/AppTest.java
@@ -0,0 +1,13 @@
+package com.vp.demo.sts_github_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
